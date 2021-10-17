fe7564e6505db4af7feba9400ffd8123cf0e73f0
Drazbaz <62464834+Drazbaz@users.noreply.github.com>
2021-10-17 08:22:32 +0100

    removed debug banner

--- lib/config/startup.dart
+++ lib/config/startup.dart
@@ -10,6 +10,7 @@ class EpisodePlus extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Episode Plus',
       theme: ThemeData(
         // This is the theme of your application.
